2a4a577b6e2dc2e7acf44de594942922be5c6160
Dominic Sauder <dominic.sauder@gmail.com>
2021-08-16 21:23:22 +0200

    the infamous initial commit

---
+++ CodeClap.Tagger/Shots/Shot.cs
@@ -0,0 +1,6 @@
+﻿
+namespace CodeClap.Tagger.Shots;
+
+public record Shot (Guid Id, string Pilot, string Craft, DateTimeOffset CreatedAt, Location Location);
+
+public record Location(double Longitude, double Latitude);
---
+++ CodeClap.Tagger/Shots/ShotStore.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+
+namespace CodeClap.Tagger.Shots;
+public class ShotStore
+{
+    private readonly ConcurrentDictionary<Guid, Shot> _store = new ();
+
+    public ShotStore()
+    {
+        var now = DateTimeOffset.Now;
+        var items = new List<Shot>
+        {
+            new Shot(Guid.NewGuid(), "Jamie", "Tinyhawk Freestyle", now.AddMinutes(-15), new Location(0, 0)),
+            new Shot(Guid.NewGuid(), "Dominic", "Marmotte 5\"", now.AddHours(-3), new Location(0, 0)),
+            new Shot(Guid.NewGuid(), "Manu", "GTR Something", now.AddDays(-2), new Location(0, 0)),
+            new Shot(Guid.NewGuid(), "Dominic", "Rubberduck-shaped quad", now.AddDays(-7), new Location(0, 0))
+        };
+
+        items.ForEach(shot => _store.AddOrUpdate(shot.Id, shot, (_, shot) => shot));
+    }
+
+    internal async Task<Shot> Get(Guid id)
+    {
+        return await Task.FromResult(_store[id]);
+    }
+    internal async Task<List<Shot>> Get()
+    {
+        return await Task.FromResult(_store.Values.ToList());
+    }
+
+    internal async Task Create(Shot shot)
+    {
+        _store.AddOrUpdate(shot.Id, shot, (_, shot) => shot);
+        await Task.CompletedTask;
+    }
+
+    internal async Task Update(Shot shot)
+    {
+        _store.AddOrUpdate(shot.Id, shot, (_, shot) => shot);
+        await Task.CompletedTask;
+    }
+
+    internal async Task Delete(Shot shot)
+    {
+        _store.Remove(shot.Id, out _);
+        await Task.CompletedTask;
+    }
+}
